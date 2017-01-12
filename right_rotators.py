fe9c2e46b1248e2b4048187b8bd37d692df9df9b
Sandeep Murthy <smurthy@protonmail.ch>
2017-01-12 23:07:13 +0000

    Right rotators + Ulam sequences

---
+++ right_rotators.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# https://projecteuler.net/problem=168
+
+def right_rotation(n):
+    s = str(n)
+    return int(''.join([c for c in s[-1] + s[:-1]]))
+
+def is_right_rotator(n):
+    """
+    An integer n is called a right rotator if it is a divisor
+    of its right-rotation, e.g. 142857 divides its right rotation
+    714285.    
+    """
+    return (right_rotation(n) % n == 0)
+
+def right_rotators(int_range=range(11, 10**6)):
+    for n in int_range:
+        if is_right_rotator(n):
+            yield n
+
+
+if __name__ == "__main__":
+    m = int(sys.argv[1].strip())
+    ans = int(str(sum(n for n in right_rotators(int_range=range(11, 10**m))))[-5:])
+    print('Last 5 digits of the sum of all right rotators in the range 10 < n < 10^{}: {}'.format(m, ans))
---
+++ ulam_sequence.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+# https://projecteuler.net/problem=167
+
+from collections import Counter
+
+def ulam(a, b, k):
+    """
+    An integer sequence defined as follows:
+ 
+        U(a, b, 1) = a
+        U(a, b, 2) = b
+        U(a, b, k: k > 2) = min{U(k-i)+U(k-j) for i, j < k s.t. i != j and sum is unique}
+ 
+        e.g. first 16 terms of the U(2, 5) sequence:
+ 
+            2, 5, 7, 9, 11, 12, 13, 15, 19, 23, 27, 29, 35, 37, 41, 43, ...
+    """
+    if k == 1:
+        return a
+    elif k == 2:
+        return b
+
+    terms = {1: a, 2: b}
+    min_bound = ulam(a, b, k - 1)
+    terms[k - 1] = min_bound
+    sums = Counter()
+
+    for i in range(1, k - 1):
+        for j in range(i + 1, k):
+            try:
+                ti = terms[i]
+            except KeyError:
+                ti = ulam(a, b, i)
+                terms[i] = ti
+            else:
+                try:
+                    tj = terms[j]
+                except KeyError:
+                    tj = ulam(a, b, j)
+                    terms[j] = tj
+
+            s = ti + tj
+            if s > min_bound:
+                sums[s] += 1
+
+    return min(s for s in sums if sums[s] == 1 and s > min_bound)
+
+
+def ulam_sequence(a, b, num_terms):
+    for k in range(1, num_terms + 1):
+        yield ulam(a, b, k)
+
+if __name__ == "__main__":
+    a, b, nt = map(int, sys.argv[1:4])
+    for u in ulam_sequence(a, b, nt):
+        print('{}', end=' ')
+
+
+
